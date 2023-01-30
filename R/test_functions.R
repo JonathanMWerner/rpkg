5f711d52415bbe52884b915462ebee057ad7485a
Werner <werner@rugen18.cshl.edu>
2023-01-30 12:56:43 -0500

    Add a test function with documentation

---
+++ R/test_functions.R
@@ -0,0 +1,13 @@
+
+#'Add together two numbers
+#' @param a A number.
+#' @param b A number.
+#' @return A number
+#' @examples 
+#' add(1, 1)
+#' add(10, 11)
+
+add = function(a, b){
+  return(a + b)
+}
+
